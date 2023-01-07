4281ad7c9f8a9e35a0bc2e0e2a32e460163d8b4c
MorenoLaQuatra <moreno.laquatra@gmail.com>
2023-01-07 10:39:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# ARCH: Audio Representations benCHmark
+
+ARCH is a benchmark for audio representations. Its goal is to provide an unified framework for the evaluation of audio representations.
+The benchmark is intended to be **easy to use** and to **allow the comparison of different audio representations**.
+
+The main features of ARCH are:
+- **Easy to use**: the benchmark is designed to be easy to use.
+- **Comprehensive evaluation**: the benchmark provides a comprehensive evaluation of audio representations. It aims at providing a unified framework for the evaluation of audio representations on a wide range of tasks.
+- **Standardization**: the benchmark wants to standardize the evaluation of audio representations. The pletora of ARL models and datasets makes it difficult to compare them. The benchmark aims at providing a standard way to evaluate audio representations.
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
+
+## Contributing
+
+TODO
+
+## License
+
+TODO
+
+## Authors
+
+TODO
+
+## Acknowledgments
+
+TODO
+
+## References
+
+TODO
+
+## Cite
+
+TODO
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='arch',
+    version='1.0',
+    description='ARCH: Audio Representations benCHmark',
+    py_modules=["arch"],
+    packages=find_packages(include=['arch', 'arch.*']),
+    classifiers={
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    },
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    install_requires = [
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "tqdm",
+        "torch",
+        "torchaudio",
+        "librosa",
+        "soundfile",
+        "pydub",
+    ],
+    extras_require = {
+        "dev" : [
+            "pytest>=3.7",
+        ],
+    },
+    url="https://github.com/MorenoLaQuatra/ARCH",
+    author="Moreno La Quatra",
+    author_email="moreno.laquatra@gmail.com",
+)
