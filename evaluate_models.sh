e13133a0731d19611db68a4bf7d48b33e4c2b0ec
MorenoLaQuatra <moreno.laquatra@gmail.com>
2023-01-17 22:04:10 +0000

    Major changes

--- evaluate_models.sh
+++ evaluate_models.sh
@@ -5,3 +5,8 @@ python evaluate_hf_models.py --model facebook/wav2vec2-large-xlsr-53 --device cu
 python evaluate_hf_models.py --model microsoft/wavlm-base --device cuda > results/wavlm-base.txt
 python evaluate_hf_models.py --model microsoft/wavlm-base-plus --device cuda > results/wavlm-base-plus.txt
 python evaluate_hf_models.py --model microsoft/wavlm-large --device cuda > results/wavlm-large.txt
+
+python evaluate_hf_models.py --model facebook/hubert-base-ls960 --device cuda > results/hubert-base-ls960.txt
+python evaluate_hf_models.py --model facebook/hubert-large-ll60k --device cuda > results/hubert-large-ll60k.txt
+python evaluate_hf_models.py --model facebook/hubert-xlarge-ll60k --device cuda > results/hubert-xlarge-ll60k.txt
+
